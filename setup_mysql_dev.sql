f564935d9d2055f64e1be314bd0c46bcfe7fb0e9
ArthurAbeshouse <artieabe99@gmail.com>
2020-03-30 17:25:39 +0000

    added comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,9 @@
+-- Prepares a MySQL server for the project
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
+-- Creates user if not found
 CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost";
-
+-- Give privileges to user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
-
+-- Gives select privileges to user
 GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
