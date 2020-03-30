c187d491df2c25bcb1bb3a8e0bf4a25482450de0
ArthurAbeshouse <artieabe99@gmail.com>
2020-03-30 17:30:09 +0000

    added flush privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,3 +7,5 @@ CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost";
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
 -- Gives select privileges to user
 GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
+
+FLUSH PRIVILEGES;
