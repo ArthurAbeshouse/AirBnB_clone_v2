65ddb27731d90624822fb528789c386b7765e82c
ArthurAbeshouse <artieabe99@gmail.com>
2020-03-30 17:59:37 +0000

    swaped code positions

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,10 +2,10 @@
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Creates user if not found
-CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost";
--- Give privileges to user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
+CREATE USER IF NOT EXISTS 'hbnb_dev"@"localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grants privileges                                                                                                                                         
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+-- Givse privileges to user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev"@"localhost';
 -- Gives select privileges to user
-GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
-
-FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev"@"localhost';
