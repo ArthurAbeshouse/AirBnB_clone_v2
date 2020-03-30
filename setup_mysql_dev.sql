60e5e16227a45a82022ea696afe6411a9b076ad7
ArthurAbeshouse <artieabe99@gmail.com>
2020-03-30 23:53:40 +0000

    reverted changes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,11 @@
 -- Prepares a MySQL server for the project
 -- Create database
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Creates user if not found
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grants privileges                                                     
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 -- Givse privileges to user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
-      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Gives select privileges to user
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
-      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,11 @@
 -- Prepares a MySQL server for the project
 -- Create database
-CREATE DATABASE IF NOT EXISTS hbnb_test_db IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Creates user if not found
 CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Grants privileges                                                     
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 -- Givse privileges to user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- Gives select privileges to user
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
