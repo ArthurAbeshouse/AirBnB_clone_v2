880198d1f63499a6d25d72031e10ff899692218a
ArthurAbeshouse <artieabe99@gmail.com>
2020-03-30 18:11:37 +0000

    fixed quotes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,7 @@
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Creates user if not found
-CREATE USER IF NOT EXISTS 'hbnb_dev"@"localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grants privileges                                                                                                                                         
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 -- Givse privileges to user
