5a729da43a29e57da8d8f9d17d0a24cbfecce25e
ArthurAbeshouse <artieabe99@gmail.com>
2020-03-30 18:15:42 +0000

    more quote fixes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,6 +6,6 @@ CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grants privileges                                                                                                                                         
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 -- Givse privileges to user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev"@"localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Gives select privileges to user
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev"@"localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
