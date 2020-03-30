ff402429225e6cc7a7d55b56571871f1e91a24fe
ArthurAbeshouse <artieabe99@gmail.com>
2020-03-30 18:21:08 +0000

    created user hbnb_test and fixed spacing

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Creates user if not found
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Grants privileges                                                                                                                                         
+-- Grants privileges                                                     
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 -- Givse privileges to user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
