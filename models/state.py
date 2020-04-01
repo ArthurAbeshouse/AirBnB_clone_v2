1bc592714b7d974de239081b9efb3a68e5012745
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-01 19:22:50 +0000

    fixed pep8 error

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models import City, storage
 
+
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
