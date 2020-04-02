195ab861207a4396fc63f8dd77e5d60d57eb9277
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-02 00:25:24 +0000

    testing

--- models/place.py
+++ models/place.py
@@ -4,14 +4,15 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Float, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
+import models
 
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
                              primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
                              primary_key=True, nullable=False))
 
 
@@ -43,11 +44,11 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        reviews = relationship('Review', cascade='all, delete',
-                               backref='place')
-        amamenities = relationship('Amenity', secondary='place_amenity',
-                                   viewonly=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade="all, delete",
+                               backref="place")
+        amamenities = relationship("Amenity", secondary="place_amenity",
+                                   viewonly=False, back_populates="place_amenity")
 
     else:
         @property
