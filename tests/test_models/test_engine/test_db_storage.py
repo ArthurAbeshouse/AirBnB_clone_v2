b3df276e8d4abed044877d661f81ee4d1ddb5619
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-02 21:21:06 +0000

    unittests

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""tests database storage"""
+import MySQLdb
+import os
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine.db_storage import DBStorage
+
+
+class TestDBStorage (unittest.TestCae):
+    """this will test DBStorage"""
+
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.user = User()
+        cls.user.first_name = "Kev"
+        cls.user.last_name = "Yo"
+        cls.user.email = "1234@yahoo.com"
+        cls.storage = DBStorage()
+
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.user
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != "db",
+                     "db isn't being used")
+
+    def test_all(self):
+        """tests all in File Storage"""
+        storage = DBStorage()
+        storage.reload()
+        len_of_dict = len(storage.all())
+        state = State(name="test_all_state")
+        state.save()
+        storage.save()
+        self.assertsIs(len(storage.all()), len_of_dict + 1)
