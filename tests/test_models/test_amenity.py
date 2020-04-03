b37c7e6d059551dc7c3ec7b8c0c5bf65cef5b73e
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-03 02:05:05 +0000

    fixes

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -53,9 +53,6 @@ class TestAmenity(unittest.TestCase):
         """test attribute type for Amenity"""
         self.assertEqual(type(self.amenity.name), str)
 
-    @unittest.skipIf(
-        os.getenv("HBNB_TYPE_STORAGE") == "db",
-        "Wrong Storage type")
     def test_save_Amenity(self):
         """test if the save works"""
         self.amenity.save()
