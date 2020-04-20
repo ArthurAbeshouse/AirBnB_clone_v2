48d7d19f07cddf1d1c72dc3898e1f4725888d2d3
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-20 20:38:32 +0000

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """ Routes an index """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ Routes /hbnb """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """ Routes /c/<text> """
+    return "C " + text.replace("_", " ")
+
+if __name__ == '__main__':
+    app.run()
+    app.url_map.strict_slashes = False
