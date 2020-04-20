51a99bc4a44cf5af0cf885c4033dd2fc5c9c671e
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-20 20:11:33 +0000

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    """ Routes an index """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run()
+    app.url_map.strict_slashes = False
