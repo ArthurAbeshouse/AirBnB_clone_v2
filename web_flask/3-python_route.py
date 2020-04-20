aaea3a8387ca9b0e86edf27e28d1d83b023b0a32
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-20 21:05:17 +0000

    Python is cool! & added permissions

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,13 @@ def c_text(text):
     """ Routes /c/<text> """
     return "C " + text.replace("_", " ")
 
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>")
+def python_text(text):
+    """ Routes /python/<text> """
+    return "Python " + text.replace("_", " ")
+
 if __name__ == '__main__':
     app.run()
     app.url_map.strict_slashes = False
