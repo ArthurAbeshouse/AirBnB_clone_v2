44cd0f8f2f5bc2fa5ecb86aa49a650518f035ad9
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-21 21:26:07 +0000

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,7 +17,8 @@ def close_db(exception):
 def states_list():
     """ Lists all the states """
     states = list(storage.all(State).values())
-    return render_template("7-states_list.html", states=states)
+    states_sorted = sorted(states, key=lambda state: state.name)
+    return render_template("7-states_list.html", states_sorted=states_sorted)
 
 
 if __name__ == '__main__':
