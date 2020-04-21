f30189e0cefaa225e7dd7800069d482bfc069209
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-21 21:07:25 +0000

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(exception):
+    """ Closes the database """
+    storage.close()
+
+
+@app.route("/states_list")
+def states_list():
+    """ Lists all the states """
+    states = list(storage.all(State).values())
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == '__main__':
+    app.run()
+    app.url_map.strict_slashes = False
