043c8d5a6fc1813dd620aea25fc93b688dd6eb34
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-22 03:06:36 +0000

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from os import getenv
 
 app = Flask(__name__)
 
@@ -17,8 +18,12 @@ def close_db(exception):
 @app.route("/states/<state_id>")
 def states_list(state_id=None):
     """ Lists all the states """
-    states = list(storage.all(State).values())
-    return render_template("9-states.html", state=states, state_id=state_id)
+    if state_id is not None:
+        states = storage.all(State)
+        state_id = "States.{}".format(state_id)
+    else:
+        states = list(storage.all(State).values())
+    return render_template("9-states.html", states=states, state_id=state_id)
 
 
 if __name__ == '__main__':
