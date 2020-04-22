90d8145acfa8998b3615a99d831aebf5272cb507
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-22 00:36:46 +0000

    States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(exception):
+    """ Closes the database """
+    storage.close()
+
+
+@app.route("/states")
+@app.route("/states/<state_id>")
+def states_list(state_id=None):
+    """ Lists all the states """
+    states = list(storage.all(State).values())
+    return render_template("9-states.html", state=states, state_id=state_id)
+
+
+if __name__ == '__main__':
+    app.run()
+    app.url_map.strict_slashes = False
